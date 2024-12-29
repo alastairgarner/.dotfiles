b3156275d5d6b6fb4cc7568173f7cf7fc3bcffc9
alastair <garneralastair@gmail.com>
2024-12-29 18:56:25 +0000

    Add ghostty to NixOS

--- .config/nixos/flake.nix
+++ .config/nixos/flake.nix
@@ -9,13 +9,23 @@
     #   url = "github:nix-community/home-manager";
     #   inputs.nixpkgs.follows = "nixpkgs";
     # };
+
+    ghostty = {
+      url = "github:ghostty-org/ghostty";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, ghostty, ... }@inputs: {
     nixosConfigurations.default = nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs;};
       modules = [
         ./configuration.nix
+
+        {
+          environment.systemPackages = [
+            ghostty.packages.x86_64-linux.default
+          ];
+        }
         # inputs.home-manager.nixosModules.default
       ];
     };
